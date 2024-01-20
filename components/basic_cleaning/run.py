2270b079c1f337d6f45aa6607c5e5843270ee226
s.y <63178306+shawnyazdani@users.noreply.github.com>
2024-01-19 23:49:40 -0800

    Fix to clean data, enforcing lat/long bounds.
    Fix to clean data, enforcing lat/long bounds.

--- components/basic_cleaning/run.py
+++ components/basic_cleaning/run.py
@@ -28,6 +28,9 @@ def go(args):
     logger.info("Converting last_review to type datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy() 
+
     logger.info("Outputting clean sample dataframe to csv")
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
